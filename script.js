f5ab034273d55c504459e80e8c270340de69946d
SyedMarufAhmad <135939210+SyedMarufAhmad@users.noreply.github.com>
2023-08-22 11:49:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+	let first= document.querySelector("#form1 input=["fname"]");
+	let last = document.querySelector("#form1 input = ["lname"]");
+	let fname=first.value;
+	let lname= last.value;
+	
+ alert(fname +" " lname);
 }
